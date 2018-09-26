91de85e897284925d84c25a59c2d1bee28110d9e
875818589@qq.com <19890909lcy>
2018-09-26 10:58:29 +0800

    spring boot 拦截器

--- src/main/java/com/example/sbdemo/WebMvcConfig.java
+++ src/main/java/com/example/sbdemo/WebMvcConfig.java
@@ -3,7 +3,12 @@ package com.example.sbdemo;
 import com.example.sbdemo.converter.StringToDateConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -12,5 +17,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new StringToDateConverter());
 
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new HandlerInterceptor() {
+            @Override
+            public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+                return false;
+            }
+        });
     }
 }

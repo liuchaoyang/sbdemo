c546fcc4821972f42d2245dee39ffb49ffa7e5d0
875818589@qq.com <19890909lcy>
2019-04-16 11:02:38 +0800

    TwitterIDGenerator

--- src/main/java/com/example/sbdemo/base/DataType.java
+++ src/main/java/com/example/sbdemo/base/DataType.java
@@ -10,10 +10,17 @@ public class DataType {
 //        System.out.println(3 >>> 1); //1
 
         int toRight = -2;
-        System.out.println(toRight >> 1); //-1
-        System.out.println(toRight >> 3); //-1
-        System.out.println(toRight >>> 1);
+//        System.out.println(toRight >> 1); //-1
+//        System.out.println(toRight >> 3); //-1
+//        System.out.println(toRight >>> 1);
 //        System.out.println(Math.pow(2, 30) + 1);
 
+//    cal:1000 0001
+//     bu:1111 1111
+//    <<5 1110 0000
+//    ^   0001 1111
+//      y:31
+        System.out.println(-1 ^ (-1 << 5)); //31
+        System.out.println(Long.toBinaryString(System.currentTimeMillis()));
     }
 }

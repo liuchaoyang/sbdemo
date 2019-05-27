4dd6ebb8dbe311b890fa969a3e5f5a7f26622b0b
875818589@qq.com <19890909lcy>
2019-05-27 09:26:38 +0800

    InvocationTest

---
+++ src/main/java/com/example/sbdemo/spring/InvocationTest.java
@@ -0,0 +1,63 @@
+package com.example.sbdemo.spring;
+
+import com.example.sbdemo.model.User;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+public class InvocationTest {
+
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+        InvocationTest base = new InvocationTest();
+        MyHello myHello = base.new MyHello();
+        ProxyHello proxyHello = base.new ProxyHello(myHello);
+
+        Class<?>[] interfaces = myHello.getClass().getInterfaces();
+        IHello proxyInstance = (IHello)Proxy.newProxyInstance(InvocationTest.class.getClassLoader(), interfaces, proxyHello);
+        proxyInstance.say();
+    }
+
+    private static void testInvoke() throws InvocationTargetException, IllegalAccessException {
+        User user = new User();
+        user.setMobile("136");
+
+        Method[] methods = User.class.getMethods();
+        for (Method method : methods) {
+            if (method.getName().equals("getMobile")) {
+                Object invoke = method.invoke(user, null);
+                System.out.println(invoke);
+            }
+        }
+    }
+
+    class ProxyHello implements InvocationHandler {
+
+        IHello iHello;
+        public ProxyHello(IHello myHello) {
+            this.iHello = myHello;
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            System.out.println("Proxy start............");
+            Object object = method.invoke(iHello, args);
+            System.out.println("Proxy end............");
+            return object;
+        }
+    }
+
+    interface IHello {
+        void say();
+    }
+
+    class MyHello implements IHello {
+
+        @Override
+        public void say() {
+            System.out.println("MyHello say....");
+        }
+    }
+
+}

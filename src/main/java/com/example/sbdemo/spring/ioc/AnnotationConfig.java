2554c3be82f415131ec06aef6dd98527724bf240
19057866 <Password2>
2021-06-15 18:23:02 +0800

    aware、listener

--- src/main/java/com/example/sbdemo/spring/ioc/AnnotationConfig.java
+++ src/main/java/com/example/sbdemo/spring/ioc/AnnotationConfig.java
@@ -1,6 +1,9 @@
 package com.example.sbdemo.spring.ioc;
 
 import com.example.sbdemo.service.UserService;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +13,27 @@ public class AnnotationConfig {
     @Bean
     public UserService myUserService() {
         return new SimpleServiceImpl();
+    }
+
+    @Bean
+    public BeanFactoryPostProcessor beanFactoryPostProcessor() {
+        return configurableListableBeanFactory -> System.out.println(configurableListableBeanFactory.getClass().getName()
+                + "@" + configurableListableBeanFactory.hashCode()
+                + " postProcessBeanFactory ------------------");
+    }
+
+    @Bean
+    public ApplicationContextAware applicationContextAware1() {
+        return applicationContext -> System.out.println(applicationContext.getClass().getName()
+                + "@" + applicationContext.hashCode()
+                + " applicationContextAware1-------------------");
+    }
+
+    @Bean
+    public ApplicationListener applicationListener1() {
+        return event -> System.out.println(event.getClass().getName()
+                + "@" + event.hashCode()
+                + " applicationListener1------------------------");
     }
 
 

ec8cecb0b9fc6e59059f5271df64cb4e61aeb9cc
19057866 <Password2>
2021-09-12 11:42:13 +0800

    javap

---
+++ src/main/java/com/example/sbdemo/jvm/javap/CompileDemo.java
@@ -0,0 +1,11 @@
+package com.example.sbdemo.jvm.javap;
+
+public class CompileDemo {
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+        int c = a + b;
+        c++;
+    }
+}
